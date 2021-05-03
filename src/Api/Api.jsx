ea0386eb56f3c6a6a217d1cfa7cc49065d998f14
depst0r <depstor90@gmail.com>
2021-05-03 15:22:51 +0300

    pokemonFetsh

--- src/Api/Api.jsx
+++
@@ -1,5 +0,0 @@
-export const fetchApi = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`)
-        .then(res => res.json())
-        .then(res => res)
-} 
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { usaeState, useState } from 'react'
 import { fetchApi } from './Api/Api'
 
 export const App = () => {
-  return <>
-    <h1>App</h1>
+  const [pokemon, setPokemon] = useState(null)
+
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=1000&offset=200`)
+    .then(res => res.json())
+    .then(res => setPokemon(res.results))
+
+  return (
+    <>
+      <div>
+        <h1>h1b</h1>
+      </div>
     </>
+  )
 }
